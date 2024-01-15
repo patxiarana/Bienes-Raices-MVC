4a89f2c1c41e0722a59f51b0a52accd7f64eb98e
patxi <patxiarana05@gmail.com>
2024-01-15 17:02:22 -0300

    Asociando URLS y los metodos del controlador

--- Router.php
+++ Router.php
@@ -1,11 +1,34 @@
-<?php 
+<?php
 
-namespace MVC ; 
+namespace MVC;
 
 
-class Router {
-    public function __construct()
+class Router
+{
+
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    public function get($url, $fn)
     {
-        echo "Creando el Router" ; 
+        $this->rutasGET[$url] = $fn;
+    }
+
+
+
+
+    public function comprobarRutas()
+    {
+        $urlActual = $_SERVER['PATH_INFO'] ?? "/";
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        if ($metodo === "GET") {
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        }
+        if ($fn) {
+           call_user_func($fn,$this) ; 
+        } else {
+            echo "Pagina no encontrada";
+        }
     }
 }
--- public/index.php
+++ public/index.php
@@ -7,3 +7,10 @@ use MVC\Router ;
 
 
 $router = new Router() ; 
+
+$router->get('/nosotros', 'funcion_nosotros') ; 
+$router->get('/tienda_virtual', 'funcion_tienda') ; 
+$router->get('/contacto', 'funcion_contacto') ; 
+$router->get('/admin', 'funcion_admin') ;
+
+$router->comprobarRutas(); 
