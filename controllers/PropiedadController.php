a4318a5cf0b01305ad4b48189f35ad161c88b898
patxi <patxiarana05@gmail.com>
2024-01-17 09:14:49 -0300

    consultado modelo en el controlador

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -2,14 +2,21 @@
 
 namespace Controllers;
 use MVC\Router ; 
+use Model\Propiedad ; 
+
 
 class PropiedadController
 {
     public static  function index(Router $router)
     {
+
+      $propiedades = Propiedad::all() ; 
+      $resultado = null ; 
+
      $router->render('propiedades/admin',[
-            
-     ]);  
+            "propiedades" => $propiedades,
+            "resultado"  => $resultado
+      ]);  
     }
 
     public static function crear(){
