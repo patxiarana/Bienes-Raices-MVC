286063b41363abaf343176df1385cf1df094ba80
patxi <patxiarana05@gmail.com>
2024-01-22 16:34:12 -0300

    verificando si el usuario existe

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,31 +1,42 @@
 <?php
 
-namespace Controllers ; 
-use MVC\Router ; 
-use Model\Admin ; 
+namespace Controllers;
+
+use MVC\Router;
+use Model\Admin;
 
 
-class LoginController {
-    public static function login (Router $router) {
-        $errores = [] ; 
+class LoginController
+{
+    public static function login(Router $router)
+    {
+        $errores = [];
 
-        if($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $auth = new Admin($_POST) ; 
-            $errores = $auth->validar() ; 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $auth = new Admin($_POST);
+            $errores = $auth->validar();
+            if (empty($errores)) {
+                //verificar si el usuario exist
+                $resultado = $auth->existeUsuario();
+
+                if (!$resultado) {
+                    $errores = Admin::getErrores();
+                } else {
+              //verificar el password
+
+              //authentica el usuario
+                }
+                
+            }
         }
-         if(empty($errores)) {
-         //verificar si el usuario exist
-
-
-         //verificar el password
-         }
 
         $router->render('auth/login', [
-         'errores' => $errores
-        ]) ; 
+            'errores' => $errores
+        ]);
     }
 
-    public static function logout () {
+    public static function logout()
+    {
         echo "Desde logout";
     }
 }
--- models/Admin.php
+++ models/Admin.php
@@ -1,33 +1,51 @@
 <?php
 
-namespace Model ; 
+namespace Model;
 
 
-class Admin extends ActiveRecord {
+class Admin extends ActiveRecord
+{
     //Base de datos 
-    protected static $table = "usuarios" ; 
-    protected static $columnasDB  = ['id', 'email','password'] ; 
+    protected static $table = "usuarios";
+    protected static $columnasDB  = ['id', 'email', 'password'];
 
-    public $id ;
-    public $email ;
-    public $password ; 
+    public $id;
+    public $email;
+    public $password;
 
 
     public function __construct($args = [])
     {
-        $this->id = $args['id'] ?? null ; 
-        $this->email = $args['email'] ?? '' ; 
-        $this->password = $args['password'] ?? '' ; 
+        //s debuguear($args['admin']['password']) ; 
+        $this->id = $args['id'] ?? null;
+        $this->email = $args['admin']['email'] ?? '';
+        $this->password = ($args['admin']['password']) ?? '';
     }
 
-    public function validar(){
-        if(!$this ->email ) {
-            self::$errores[] = "el email es obligatorio" ; 
+    public function validar()
+    {
+        if (!$this->email) {
+            self::$errores[] = "el email es obligatorio";
         }
-        if(!$this ->password ) {
-            self::$errores[] = "el password es obligatorio" ; 
+        if (!$this->password) {
+            self::$errores[] = "el password es obligatorio";
         }
 
-        return self::$errores; 
+        return self::$errores;
+    }
+
+    public function existeUsuario()
+    {
+        //Revisar si un usuario existe o no 
+        $query = "SELECT * FROM usuarios WHERE email = '" . $this->email . "' LIMIT 1";
+
+
+        $resultado = self::$db->query($query);
+
+        if (!$resultado->num_rows) {
+            self::$errores[] = 'el usuario no existes';
+            return;
+        }
+        return $resultado ; 
     }
 }
